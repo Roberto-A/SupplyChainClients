b3f6501641633673f2ac7453d2904c9387e33fcf
Roberto <ravila@pdx.edu>
2018-04-15 02:09:27 -0700

    Added a generic web form to create a new generic asset.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+
+import Form from './Form';
 
 class App extends Component {
   render() {
@@ -8,11 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Asset Tracker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <Form/>
+        </div>
       </div>
     );
   }
---
+++ src/Form.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './styles/Form.css';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            sku: '',
+            model: '',
+            size: '',
+            manufactureDate: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(event) {
+      alert(
+        "Product Info\nID: " + this.state.id +
+        "\nSKU: " + this.setState.sku +
+        "\nModel: " + this.state.model +
+        "\nSize: " + this.state.size + 
+        "\nManufacture Date: " + this.state.manufactureDate
+      );
+
+      alert("Stringify\n" + JSON.stringify(this.state));
+      event.preventDefault();
+    }
+
+    render() {
+        return (
+          <div className="Form">
+            <form onSubmit={this.handleSubmit}>
+              <legend>New Item</legend>
+              <div className="form-group">
+                <label>Product ID</label>
+                <input 
+                  type="text" 
+                  name="id"
+                  placeholder="Product ID"
+                  onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label>SKU:&emsp;</label>
+                <input 
+                  type="text" 
+                  name="sku" 
+                  placeholder="SKU"
+                  onChange={this.handleChange}/>
+              </div>
+              <div className="row">
+                <div className="col-md">
+                  <div className="form-group">
+                    <label>Model</label>
+                    <input 
+                      type="text" 
+                      name="model" 
+                      placeholder="Model"
+                      onChange={this.handleChange}/>
+                  </div>
+                </div>
+                <div className="col-md">
+                  <div className="form-group">
+                    <label>Size</label>
+                    <input 
+                      type="number" 
+                      min="6" 
+                      max="14" 
+                      placeholder="Size"
+                      name="size" 
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-md">
+                  <div className="form-group">
+                    <label>Manu Date</label>
+                    <input 
+                      type="date" 
+                      name="manufactureDate" 
+                      placeholder="Manufature Date"
+                      onChange={this.handleChange}/>
+                  </div>
+                </div>
+              </div>
+              <div className="btn-submit">
+                <input type="submit" value="Submit" />
+              </div>
+            </form>
+          </div>
+        );
+      }
+}
+
+export default Form;
